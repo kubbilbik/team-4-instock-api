c7b082b79fdfceccc97c54ea7ee2133d98b4339e
kubbilbik <76406996+kubbilbik@users.noreply.github.com>
2024-01-18 12:56:32 -0500

    create an endpoint that will allow users to send a GET request for a list of warehouses.

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+const warehousesRouter = require('./routes/warehouses');
+app.use('/warehouses', warehousesRouter);
+
+const defaultPort = 3306;
+const alternativePort = 3000; 
+const port = process.env.PORT == defaultPort ? alternativePort : process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
