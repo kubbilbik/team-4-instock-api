ca47e096fd5e037197010977ae71549a0b03d326
abdulghfour <abdulghfour.alhalabi@gmail.com>
2024-01-18 13:33:14 -0500

    get_all_inventory

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const knex = require("knex")(require("./knexfile"));
+const PORT = process.env.PORT || 8080;
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+
+const inventoryRoutes = require('./routes/inventory');
+
+
+app.use('/inventories', inventoryRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port: ${PORT}`);
+  });  
