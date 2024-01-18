1e8de0892490f75bd84f4179d103b82fa4246e77
kubbilbik <76406996+kubbilbik@users.noreply.github.com>
2024-01-18 13:02:38 -0500

    port changes

--- index.js
+++ index.js
@@ -10,9 +10,8 @@ app.use(express.json());
 const warehousesRouter = require('./routes/warehouses');
 app.use('/warehouses', warehousesRouter);
 
-const defaultPort = 3306;
-const alternativePort = 3000; 
-const port = process.env.PORT == defaultPort ? alternativePort : process.env.PORT || 3000;
+
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
