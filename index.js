c421f32df45f34df3132af2c176318764c6dfcad
Taylor Sabbag <taylorsabbag@gmail.com>
2024-01-18 13:54:57 -0500

    Fix inventoryRoutes to inventoryRouter to fit import.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/inventories', inventoryRoutes);
+app.use('/api/inventories', inventoryRouter);
 app.use("/api/warehouses", warehousesRouter);
 
 app.listen(PORT, console.log(`Server running on http://localhost:${PORT}`));
